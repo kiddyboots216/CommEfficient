a40ff680b8282f5f51ac3b29e64874dce3399048
kiddyboots216 <kiddyboots216@berkeley.edu>
2020-02-05 18:46:05 -0800

    I can't believe I get paid to do this

--- CommEfficient/cv_train.py
+++ CommEfficient/cv_train.py
@@ -229,7 +229,7 @@ def run_batches(model, opt, lr_scheduler, loader,
             loss, acc, download, upload = model(batch)
             if np.mean(loss) > args.nan_threshold:
                 print(f"LOSS OF {loss} EXCEEDS {args.nan_threshold}, TERMINATING TRAINING")
-            return np.nan, np.nan, np.nan, np.nan
+                return np.nan, np.nan, np.nan, np.nan
 
             client_download += download
             client_upload += upload
